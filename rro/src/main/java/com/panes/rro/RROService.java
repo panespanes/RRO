401a16dbe9a1a6862322a6b4aca21ac65631f0a6
panes <panestao@gmail.com>
2016-08-04 22:02:15 +0800

    add request callback

--- rro/src/main/java/com/panes/rro/RROService.java
+++ rro/src/main/java/com/panes/rro/RROService.java
@@ -1,11 +1,14 @@
 package com.panes.rro;
 
+import android.os.Handler;
 import android.util.Log;
 
 import java.io.IOException;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
@@ -16,8 +19,11 @@ import retrofit2.http.Path;
  */
 public class RROService {
     public static final String API_URL = "https://api.github.com";
-
-    public void get() {
+    public String ret = null;
+    public interface IOnResult{
+        void onReceive(String msg);
+    }
+    public void get(final IOnResult onResult) {
         // Create a very simple REST adapter which points the GitHub API.
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(API_URL)
@@ -31,35 +37,54 @@ public class RROService {
         Call<List<Contributor>> call = github.contributors("square", "retrofit");
 
         // Fetch and print a list of the contributors to the library.
-        List<Contributor> contributors = null;
-        try {
-            contributors = call.execute().body();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        for (Contributor contributor : contributors) {
-            System.out.println(contributor.login + " (" + contributor.contributions + ")");
-            Log.d("RRO", contributor.login + "("+contributor.contributions+")");
-        }
+//        List<Contributor> contributors = null;
+//        try {
+//            contributors = call.execute().body();
+        call.enqueue(new Callback<List<Contributor>>() {
+            @Override
+            public void onResponse(Call<List<Contributor>> call, Response<List<Contributor>> response) {
+                List<Contributor> contributors = response.body();
+                for (Contributor contributor : contributors) {
+                    System.out.println(contributor.login + " (" + contributor.contributions + ")");
+
+                    Log.d("RRO", contributor.login + "(" + contributor.contributions + ")");
+                    onResult.onReceive(contributor.login);
+                }
+
+                Log.d("RRO", "onresponse");
+            }
+
+            @Override
+            public void onFailure(Call<List<Contributor>> call, Throwable t) {
+                Log.d("RRO", "onFail");
+            }
+        });
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        for (Contributor contributor : contributors) {
+//            System.out.println(contributor.login + " (" + contributor.contributions + ")");
+//            Log.d("RRO", contributor.login + "("+contributor.contributions+")");
+//        }
     }
 
 
-public static class Contributor {
-    public final String login;
-    public final int contributions;
+    public static class Contributor {
+        public  String login;
+        public  int contributions;
 
-    public Contributor(String login, int contributions) {
-        this.login = login;
-        this.contributions = contributions;
+        public Contributor(String login, int contributions) {
+            this.login = login;
+            this.contributions = contributions;
+        }
     }
-}
 
-public interface GitHub {
-    @GET("/repos/{owner}/{repo}/contributors")
-    Call<List<Contributor>> contributors(
-            @Path("owner") String owner,
-            @Path("repo") String repo);
+    public interface GitHub {
+        @GET("/repos/{owner}/{repo}/contributors")
+        Call<List<Contributor>> contributors(
+                @Path("owner") String owner,
+                @Path("repo") String repo);
 
-}
+    }
 
 }
